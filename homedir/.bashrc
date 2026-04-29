ed577ad3b5cdf97e02a816a8c7eeeb98f4f3dcf9
Nicolas Berger <nicolas.berger@cern.ch>
2026-04-29 10:54:28 +0200

    update configuration

--- homedir/.bashrc
+++ homedir/.bashrc
@@ -13,7 +13,3 @@ export LANG=C.utf8
 export PS1="[\u] \w > "
 export EDITOR=nano
 
-# setup LCG
-. /cvmfs/sft.cern.ch/lcg/views/LCG_106/x86_64-el9-gcc13-opt/setup.sh
-
-
--- homedir/setup.sh
+++ homedir/setup.sh
@@ -1,3 +1,7 @@
-chown -R mcuser:mcuser /home/mcuser
-cd /home/mcuser
-su mcuser
+# Update permissions just in case (docker sometimes messes with this)
+sudo chown -R mcuser:mcuser /home/mcuser
+
+# Set up LCG from CVMFS
+. /cvmfs/sft.cern.ch/lcg/views/LCG_106/x86_64-el9-gcc13-opt/setup.sh
+
+
